6985b0945bdbd7e391c1293c1ba29f0392598d8d
hamdiRH <49039080+hamdiRH@users.noreply.github.com>
2020-11-10 12:47:40 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'mon-projet-angular';
+  todo: String = "";
+  todolists = [];
+  onAddTodo() {
+    this.todolists = [
+      ...this.todolists,
+      { id: Math.random(), name: this.todo },
+    ];
+    this.todo = "";
+  }
+  onRemoveToDo(id, i) {
+    this.todolists = this.todolists.filter((el, index) => index !== i);
+  }
+  save(event: Event) {
+    this.todolists = [
+      ...this.todolists,
+      { id: Math.random(), name: (<HTMLInputElement>event.target).value },
+    ];
+    this.todo = "";
+    console.log(this.todolists);
+  }
+  getColor(id) {
+    return id % 2 ? "red" : "blue";
+  }
 }
